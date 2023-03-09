4e20723df9965c98e7f3ffb86f88ac60e8ae951c
Shumer-1 <borisov1908vlad@mail.ru>
2023-03-09 22:09:39 +0300

    Sec commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,28 @@
+<script setup>
+    import {reactive, ref, watchEffect} from "vue"; 
+
+    const counter = ref(0);
+    const date = ref(new Date().toLocaleTimeString());
+
+    function counterHandler(event){
+        console.log();
+    }
+
+
+    watchEffect(() => {
+        setInterval(() => {
+            date.value = new Date().toLocaleTimeString();
+        }, 1000)
+    });
+</script>
+
+<template>
+        <button @click="counter++">counter {{ counter }}</button>
+        <h1>{{ date }}</h1>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
