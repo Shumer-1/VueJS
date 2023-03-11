344a1238d8e5bb859acf116a5cd565fc488deb6a
Shumer-1 <borisov1908vlad@mail.ru>
2023-03-11 22:24:41 +0300

    Rename and delete

---
+++ HomeWork#1/src/components/Counter.vue
@@ -0,0 +1,33 @@
+<script setup>
+    import {ref, watchEffect} from "vue"; 
+
+    const counter = ref(0);
+    const date = ref(new Date().toLocaleTimeString());
+    const counterReverse = ref(100);
+    const dateNew = ref(new Date().toDateString());
+
+    watchEffect(() => {
+        setInterval(() => {
+            date.value = new Date().toLocaleTimeString();
+        }, 1000)
+    });
+    watchEffect(()=>{
+        setInterval(()=>{
+            dateNew.value = new Date().toDateString();
+        }, 1000*60*60*12)
+    });
+
+</script>
+
+<template>
+        <button @click="counter++">counter {{ counter }}</button>
+        <h1>{{ date }}</h1>
+        <h2>{{ dateNew }}</h2>
+        <button @click="counterReverse--">counterReverse {{ counterReverse }}</button>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
