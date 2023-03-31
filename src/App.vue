1093fe37b0f32fbedccbb706096e87c6e7847d2a
Shumer-1 <borisov1908vlad@mail.ru>
2023-03-31 23:29:08 +0300

    Watch+WatchEffect+ref

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,48 @@
 <script setup>
-  import { ref } from "vue";
-  import css from "./App.module.css";
+  import { 
+    ref,
+    reactive,
+    watchEffect,
+    watch } from "vue";
   
+
   const text = ref("");
-  const cards = ref(["card123", "card289", "card356", "card417", "card505"]);
-  const obj = ref([{value_1: 12, value: "first"}, {value_1:22, value: "second"}, {value_1:32, value: "third"}]);
+  const answer = ref("");
+  const answerImg = ref("");
+
+
+  watch(text, async ()=>{
+    console.log("watch");
+    if (text.value.indexOf("?") === -1){
+      answer.value = "Это не вопрос"; 
+      return;
+    }
+    const res = await fetch("https://yesno.wtf/api");
+    const data = await res.json();
+    answer.value = data.answer;
+    answerImg.value = data.image;
+  });
+
+  watchEffect(async ()=>{
+    console.log("watchEffect");
+    if (text.value.indexOf("?") === -1){
+      answer.value = "Это не вопрос"; 
+      return;
+    }
+    const res = await fetch("https://yesno.wtf/api");
+    const data = await res.json();
+    answer.value = data.answer;
+    answerImg.value = data.image;
+  });
+  function handler(el){
+    el.value = Math.random();
+  }
+
 </script>
 
 <template>
-  <input type="text" v-model="text"/>
-  <template v-for="(card, index) in cards" v-if="text.includes('show')">
-    <h1 v-if="card.includes('1') || text === 'show me all'" :class="css.myClassH1">{{ index }}. {{ card }}</h1>
-    <div v-for="value in obj">
-      <p v-if="value.value_1 === 12" :class="css.myClass_1">{{ value.value }}</p>
-      <p v-else-if="value.value_1 === 22" :class="css.myClass_2">{{ value.value }}</p>
-      <p v-else :class="css.myClass_3">{{ value.value }}</p>
-    </div>
-  </template>
-  
+  <input v-model="text" placeholder="Введите ваш вопрос"/>
+  <p v-if="answer">{{ answer }}</p>
+  <img v-if="answerImg" :src="answerImg" :alt="answer"/>
+  <input v-for="item in 3" :ref="handler"/>
 </template>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { 
+    ref,
+    reactive,
+    onMounted, 
+    onBeforeMount, 
+    onBeforeUpdate, 
+    onUpdated, 
+    onBeforeUnmount, 
+    onUnmounted } from "vue";
+onBeforeMount(() => console.log("beforeMount"));
+onMounted(() => console.log("mounted"));
+onBeforeUpdate(() => console.log("beforeUpdate"));
+onUpdated(() => console.log("updated"));
+onBeforeUnmount(() => console.log("beforeUnmount"));
+onUnmounted(() => console.log("unmounted"));
+</script>
+
+<template>
+    <div>1</div>
+</template>
