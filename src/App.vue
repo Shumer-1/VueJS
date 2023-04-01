e6052b64378f281245983b1185ea7d73f468e824
Shumer-1 <borisov1908vlad@mail.ru>
2023-04-01 21:42:45 +0300

    DZ3-third_try

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,17 @@
   import Cards from "./components/Cards/Cards.vue"
 
     const cards = ["card_1", "card_2", "card_3"];
+    const text = ref("");
 
 
 </script>
 
 <template>
-  <template  v-for="card in cards">
-    <Cards>
-        <template #slot_2 >
-          <div :class="css.myClass_1">{{ card }}</div>   
+  <input v-model="text"/>
+  <template v-for="card in cards">
+    <Cards v-if="text === 'show me cards'">
+        <template #slot_2>
+          <div :class="css.myClass_1" >{{ card }}</div>   
         </template>
         <template #slot_1> {{ card }} </template>
     </Cards>
