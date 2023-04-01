6592cf74aa7aac859b1e5a4f24f13db25ca2de63
Shumer-1 <borisov1908vlad@mail.ru>
2023-04-01 11:51:44 +0300

    DZ3-second_try

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,10 @@
 <template>
   <template  v-for="card in cards">
     <Cards>
+        <template #slot_2 >
+          <div :class="css.myClass_1">{{ card }}</div>   
+        </template>
         <template #slot_1> {{ card }} </template>
-        <template #slot_2 > {{ card }} </template>
     </Cards>
   </template>
 </template>
