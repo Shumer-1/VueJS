486eafe8812cda2a273dd2ff9abed331f5d45555
Shumer-1 <borisov1908vlad@mail.ru>
2023-04-16 09:30:17 +0300

    Модальное окно, образец с урока

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,10 @@
 <script setup>
- import {ref, Transition} from "vue";
- import Header from "./components/Header/Header.vue";
- import Input from "./components/Input/Input.vue";
-
+ import { Teleport, ref } from "vue";
+ import Modal from "./components/Modal/Modal.vue"
  const isShow = ref(false);
- const text = ref("");
-
- function a(){
-  console.log("Click - take this digit:");
-  isShow.value = !isShow.value;
- };
 </script>
 
-
 <template>
-  <button @click="a">Click me</button><br>
-  <Transition mode="out-in">
-    <component :is="isShow ? Header: Input"></component>
-  </Transition>
+  <button @click="isShow = !isShow">Показать</button>
+  <Modal :show="isShow" @close="isShow = false">Контент модального окна</Modal>
 </template>
-
-<style>
-  .v-enter-active, 
-  .v-leave-active{
-    transition: all 400ms ease;
-  }
-
-  .v-enter-from{
-    opacity: 0;
-    transform: translateX(-100px);
-  }
-  .v-leave-to{
-    opacity: 0;
-    transform: translateX(200px);
-  }
-</style>
